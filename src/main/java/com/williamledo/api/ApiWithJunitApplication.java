6b819e4803165a2fce0696097ce171d547164632
William Lêdo <williamledoc@gmail.com>
2022-06-07 15:21:37 -0300

    class user

--- src/main/java/com/williamledo/api/ApiWithJunitApplication.java
+++ src/main/java/com/williamledo/api/ApiWithJunitApplication.java
@@ -3,11 +3,16 @@ package com.williamledo.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.williamledo.api.domain.User;
+
 @SpringBootApplication
 public class ApiWithJunitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiWithJunitApplication.class, args);
+		
+		User user = new User(1, "William", "email@gmail.com", "1234");
+		
 	}
 
 }
